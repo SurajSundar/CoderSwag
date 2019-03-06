91a2db800fb07ddfa711e13e2930454c55357f0c
Venkat Suraj S <venkat.suraj@sixt.com>
2019-03-06 14:31:34 +0530

    Added the logic to show the list items based on the category values

--- app/src/main/java/com/surajsundarv/coderswag/Controllers/MainActivity.kt
+++ app/src/main/java/com/surajsundarv/coderswag/Controllers/MainActivity.kt
@@ -3,6 +3,7 @@ package com.surajsundarv.coderswag.Controllers
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.surajsundarv.coderswag.Adapters.CategoryAdapter
 import com.surajsundarv.coderswag.Models.Category
 import com.surajsundarv.coderswag.R
 import com.surajsundarv.coderswag.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
