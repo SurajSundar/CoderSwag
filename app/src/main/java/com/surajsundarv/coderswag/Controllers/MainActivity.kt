77959d36520bb9e593d2dfd051bce385438f3a5e
Venkat Suraj S <venkat.suraj@sixt.com>
2019-03-06 14:06:45 +0530

    Added list view and list item view to the project

--- app/src/main/java/com/surajsundarv/coderswag/Controllers/MainActivity.kt
+++ app/src/main/java/com/surajsundarv/coderswag/Controllers/MainActivity.kt
@@ -2,12 +2,21 @@ package com.surajsundarv.coderswag.Controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.surajsundarv.coderswag.Models.Category
 import com.surajsundarv.coderswag.R
+import com.surajsundarv.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/surajsundarv/coderswag/Models/Category.kt
+++ app/src/main/java/com/surajsundarv/coderswag/Models/Category.kt
@@ -1,3 +1,7 @@
 package com.surajsundarv.coderswag.Models
 
-class Category constructor(val title: String, val image: String)
+class Category constructor(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
